c7542b238480af9ac79fef7fd3ffafea0423c61f
Patrick Burrell <patrick.burrell@amway.com>
2020-09-30 12:49:50 -0400

    Nikola auto commit.
    Nikola version: 8.1.1

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Patrick Burrell"  # (translatable)
 BLOG_TITLE = "Small Block Software"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://smallblocksoftware.com/"
+SITE_URL = "https://www.smallblocksoftware.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://smallblocksoftware.com/"
